49fe550b5b6d6b0846cf3aea7d54770eec2beda9
Patrik Toivonen <58030020+MisterloneGR@users.noreply.github.com>
2021-11-04 21:05:39 +0200

    Add files via upload

---
+++ harjoitussivu4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace harjoitussivu4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            List<int> lista = new List<int>();
+            lista.Add(x);
+            lista.Add(y);
+
+            lista.Add(z);
+            lista.Add(b);
+            lista.Add(a);
+            int maxArvo = lista.Max();
+            Console.WriteLine(maxArvo.ToString());
+        }
+    }
+}
