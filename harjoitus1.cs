cccf65da0f4c8fa5933624685918802244389a52
Patrik Toivonen <58030020+MisterloneGR@users.noreply.github.com>
2021-11-19 06:29:49 +0200

    Add files via upload

---
+++ harjoitus1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace herjoitus1
+{
+    public partial class Harjoitus1 : Form
+    {
+        public Harjoitus1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        OtsikkoLB. = "Heippa maailma";
+
+        private void VaihdaBT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OtsikkoLB_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
