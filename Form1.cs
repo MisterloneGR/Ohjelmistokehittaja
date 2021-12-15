009f33d11ab38530d837f7df4b2a901faca64b7e
Patrik Toivonen <58030020+MisterloneGR@users.noreply.github.com>
2021-12-15 09:54:40 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+        }
+
+        private void LogIn_Click(object sender, EventArgs e)
+        {
+            if (userNameBox.Text == "" || passwordBox.Text == "")
+            {
+                MessageBox.Show("Please provide UserName and Password");
+                return;
+            }
+            try
+            {
+                //Create SqlConnection
+                string cs = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\MyDatabase.mdf;Integrated Security=True;";
+                SqlConnection con = new SqlConnection(cs);
+                SqlCommand cmd = new SqlCommand("Select * from tbl_Login where UserName=@username and Password=@password", con);
+                cmd.Parameters.AddWithValue("@username", userNameBox.Text);
+                cmd.Parameters.AddWithValue("@password", passwordBox.Text);
+                con.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapt.Fill(ds);
+                con.Close();
+                int count = ds.Tables[0].Rows.Count;
+                //If count is equal to 1, than show frmMain form
+                if (count == 1)
+                {
+                    MessageBox.Show("Login Successful!");
+                    this.Hide();
+                    frmMain fm = new frmMain();
+                    fm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace hotelliprojekti
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (UsernameTB.Text == "" || PasswordTB.Text == "")
+            {
+            }
+            MessageBox.Show("Please provide UserName and Password");
+            return;
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("UserAdd", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@LastName", txtLastName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Contact", txtContact.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Registration is successfull");
+                clear();
+            }
+        }
+        void clear()
+        {
+            txtFirstName.Text = txtLastName.Text = txtAddress.Text = txtUsername.Text = txtPassword.Text = txtConfirmPassword.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    else
+{
+    MessageBox.Show("Login Failed!");
+}
+            }
+            catch (Exception ex)
+{
+    MessageBox.Show(ex.Message);
+}
+
+        }
+    }
+}
+
